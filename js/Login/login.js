265abf0fa4bc36d4fe790b48abe12076eedd4740
Andres9312 <andres00fonseca@gmail.com>
2021-10-22 09:56:07 -0500

    // Cambios en editarPaciente/ index/ login

--- js/Login/login.js
+++ js/Login/login.js
@@ -1,43 +1,66 @@
-const loginService = {
-    API: "http://localhost:8080/api/login",
-    HEADERS: {
-        "Content-Type": "application/json",
-    },
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
-    async login() {
+const loginService = {
+      API: "http://localhost:8080/api/login",
+      HEADERS: {
+        "Content-Type": "application/json",
+      },
+
+      async login() {
 
         const cedula = document.getElementById("cedula").value;
         const clave = document.getElementById("clave").value;
         const doctor = {
-            cedula: cedula,
-            clave: clave
+          cedula: cedula,
+          clave: clave
         }
 
         const respuesta = await fetch(this.API + "/login", {
-            method: "POST",
-            headers: this.HEADERS,
-            body: JSON.stringify(doctor),
+          method: "POST",
+          headers: this.HEADERS,
+          body: JSON.stringify(doctor),
         });
 
-        
+
 
 
         if (respuesta.status === 403) {
-            alert("CAMPOS VACIOS Y/O INCORRECTOS")
-            location.reload();
-            return;
+            swal({
+              title: "Error",
+              text: "DATOS INCORRECTOS Y/O VACIOS",
+              icon: "error", 
+              dangerMode: true,
+            })
+              .then((willDelete) => {
+                if (willDelete) {
+                  location.reload();
+                    return;
+                } 
+              });
         }
 
         const usuario = await respuesta.json();
-           alert("BIENVENIDO")
-        window.location.href = "doctors.html";
-        return;
+        //alert("BIENVENIDO")
+        swal({
+              title: "BIENVENIDO",
+              icon: "success", 
+              dangerMode: true,
+            })
+              .then((willDelete) => {
+                if (willDelete) {
+                  window.location.href = "doctors.html";
+                   return;
+                } 
+              });
+       
+      }
+
 
 
 
     }
 
-}
-function login() {
-    loginService.login();
-}
+    function login() {
+      loginService.login();
+    }
+ 
